0c063a723a5cf14de66746016b75922cadfa1681
friketrike <friketrike@gmail.com>
2016-03-05 05:09:36 -0500

    Completed HW - still missing Task 3

--- src/task1/ReaderThread.java
+++ src/task1/ReaderThread.java
@@ -1,26 +1,48 @@
 package task1;
 
+import java.io.RandomAccessFile;
+import java.io.IOException;
+
 public class ReaderThread extends Thread{
+	
+	private static final String fileName = "Task1.txt";
+	
+	private static final int ALPHABET_LENGTH = 26;
+	
+	public static int readIndex = 0;
 	
 	private FileControl fc = new FileControl();
 	
 	public void run(){
-		for(int i = 0; i < 3; i++){// TODO revert to 1000
+		for(int i = 0; i < 1000; i++){
 			try{
 				fc.readerEntry();
 			}catch(InterruptedException e){System.err.println(e);}
 			
 			/*-------------<<<READING>>>---------*/
 			System.out.println("Reader thread "+Thread.currentThread().getId()+": reading...        "+fc.printInfo());
-			
-			try{
-				sleep(50);
-			}catch(InterruptedException e){
-				System.err.println(e);
-			}
+			readOneAlphabet();
+
 			try{
 				fc.readerExit();
 			}catch(InterruptedException e){System.err.println(e);}
 		}
 	}
+    
+    private void readOneAlphabet(){
+		byte[] alphabet = new byte[ALPHABET_LENGTH];
+		int status = -1;
+		try{
+			RandomAccessFile reader = new RandomAccessFile(fileName, "r");
+			reader.seek(readIndex);
+			status = reader.read(alphabet, 0, ALPHABET_LENGTH);
+			reader.close();
+		}catch(IOException e){
+			System.err.println(e);
+		}
+		if(status != -1){
+			System.out.println(new String(alphabet)); 
+			readIndex += ALPHABET_LENGTH;
+		}
+    }
 }
--- src/task2/WriterThread.java
+++ src/task2/WriterThread.java
@@ -1,26 +1,47 @@
 package task2;
 
+import java.io.RandomAccessFile;
+import java.io.IOException;
+
 public class WriterThread extends Thread{
 	
+	private static final String fileName = "Task2.txt";
+	
+	private static final int ALPHABET_LENGTH = 26;
+	
+	public static int writeIndex = 0;
+
 	private FileControl fc = new FileControl();
 	
 	public void run(){
-		for(int i = 0; i < 3; i++){ // TODO revert to 1000
+		for(int i = 0; i < 1000; i++){
 			try{
 				fc.writerEntry();
 			}catch(InterruptedException e){System.err.println(e);}
 			
 			/*-------------<<<WRITING>>>---------*/
 			System.out.println("Writer thread "+Thread.currentThread().getId()+": writing...        "+fc.printInfo());
-			
-			try{
-				sleep(50);
-			}catch(InterruptedException e){
-				System.err.println(e);
-			}
+			writeOneAlphabet();
+
 			try{
 				fc.writerExit();
 			}catch(InterruptedException e){System.err.println(e);}
 		}
 	}
+	
+	private void writeOneAlphabet(){
+		byte[] alphabet = new byte[ALPHABET_LENGTH];
+		for(byte c = 'a'; c <= 'z'; c++)
+			alphabet[(int)(c-'a')] = c;
+		try{
+			RandomAccessFile writer = new RandomAccessFile(fileName, "rw");
+			writer.seek(writeIndex);
+			writer.write(alphabet, 0, ALPHABET_LENGTH);
+			writer.close();
+		}catch(IOException e){
+			System.err.println(e);
+		}
+		writeIndex += ALPHABET_LENGTH;
+    }
 }
+
